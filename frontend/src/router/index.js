7f9c279712c183e16d82ca59311630c01968f7d5
NDRBAX <andre.bacaliu@icloud.com>
2022-11-03 09:46:29 +0100

    Login route added

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,11 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import PricingView from "../views/PricingView.vue";
+import LoginView from "../views/LoginView.vue";
 
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-  mode: "history",
-  base: import.meta.env.BASE_URL,
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
@@ -16,10 +15,18 @@ const router = new VueRouter({
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: AboutView,
+    },
+
+    {
+      path: "/pricing",
+      name: "pricing",
+      component: PricingView,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
     },
   ],
 });
