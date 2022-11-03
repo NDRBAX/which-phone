4e662d27f73501e327ba2e892c946791a3a23041
NDRBAX <andre.bacaliu@icloud.com>
2022-11-03 09:45:53 +0100

    Home view created

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <HeroSection />
+  <ServiceStats />
+  <GuideSteps />
 </template>
+
+<script setup>
+import HeroSection from "../components/HeroSection.vue";
+import ServiceStats from "../components/ServiceStats.vue";
+import GuideSteps from "../components/GuidSteps.vue";
+</script>
