c62072076a84914dbfe2071cd9d1312b1c3c8fda
NDRBAX <andre.bacaliu@icloud.com>
2022-11-04 02:44:18 +0100

    Component that display few stats of this service created

--- frontend/src/components/ServiceStats.vue
+++ frontend/src/components/ServiceStats.vue
@@ -1,106 +1,21 @@
 <template>
   <div
-    class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10"
+    class="px-4 py-0 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-0"
   >
-    <div class="grid gap-8 sm:grid-cols-2 lg:grid-cols-4">
-      <div class="text-center">
+    <div class="stat-container grid gap-8 sm:grid-cols-2 lg:grid-cols-4">
+      <div v-for="item in stats" :key="item.id" class="text-center stat-item">
         <div
-          class="flex items-center justify-center w-10 h-10 mx-auto mb-3 rounded-full bg-indigo-50 sm:w-12 sm:h-12"
+          class="flex items-center justify-center w-30 h-30 mx-auto mb-3 rounded-full bg-green-50 sm:w-12 sm:h-12"
         >
-          <svg
-            class="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10"
-            stroke="currentColor"
-            viewBox="0 0 52 52"
-          >
-            <polygon
-              stroke-width="3"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              fill="none"
-              points="29 13 14 29 25 29 23 39 38 23 27 23"
-            ></polygon>
-          </svg>
+          <font-awesome-icon
+            class="w-8 h-8 text-green-400 sm:w-10 sm:h-10"
+            :icon="item.icon"
+          />
         </div>
-        <h6 class="text-4xl font-bold text-deep-purple-accent-400">819</h6>
-        <p class="mb-2 font-bold text-md">Downloads</p>
+        <h6 class="text-4xl font-bold text-green-400">{{ item.count }}</h6>
+        <p class="mb-2 font-bold text-md">{{ item.title }}</p>
         <p class="text-gray-700">
-          It’s something that’s many of the wisest people in history have kept
-          in mind.
-        </p>
-      </div>
-      <div class="text-center">
-        <div
-          class="flex items-center justify-center w-10 h-10 mx-auto mb-3 rounded-full bg-indigo-50 sm:w-12 sm:h-12"
-        >
-          <svg
-            class="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10"
-            stroke="currentColor"
-            viewBox="0 0 52 52"
-          >
-            <polygon
-              stroke-width="3"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              fill="none"
-              points="29 13 14 29 25 29 23 39 38 23 27 23"
-            ></polygon>
-          </svg>
-        </div>
-        <h6 class="text-4xl font-bold text-deep-purple-accent-400">1.3K</h6>
-        <p class="mb-2 font-bold text-md">Users</p>
-        <p class="text-gray-700">
-          For many men, the acquisition of wealth does not end their troubles,
-          it only changes them.
-        </p>
-      </div>
-      <div class="text-center">
-        <div
-          class="flex items-center justify-center w-10 h-10 mx-auto mb-3 rounded-full bg-indigo-50 sm:w-12 sm:h-12"
-        >
-          <svg
-            class="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10"
-            stroke="currentColor"
-            viewBox="0 0 52 52"
-          >
-            <polygon
-              stroke-width="3"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              fill="none"
-              points="29 13 14 29 25 29 23 39 38 23 27 23"
-            ></polygon>
-          </svg>
-        </div>
-        <h6 class="text-4xl font-bold text-deep-purple-accent-400">91</h6>
-        <p class="mb-2 font-bold text-md">Subscribers</p>
-        <p class="text-gray-700">
-          It's a helluva start, being able to recognize what makes you happy
-          today, in this moment.
-        </p>
-      </div>
-      <div class="text-center">
-        <div
-          class="flex items-center justify-center w-10 h-10 mx-auto mb-3 rounded-full bg-indigo-50 sm:w-12 sm:h-12"
-        >
-          <svg
-            class="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10"
-            stroke="currentColor"
-            viewBox="0 0 52 52"
-          >
-            <polygon
-              stroke-width="3"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              fill="none"
-              points="29 13 14 29 25 29 23 39 38 23 27 23"
-            ></polygon>
-          </svg>
-        </div>
-        <h6 class="text-4xl font-bold text-deep-purple-accent-400">52</h6>
-        <p class="mb-2 font-bold text-md">Products</p>
-        <p class="text-gray-700">
-          Happiness is when what you think, what you say, and what you do are in
-          harmony.
+          {{ item.description }}
         </p>
       </div>
     </div>
@@ -108,7 +23,70 @@
 </template>
 
 <script>
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { inject } from "vue";
+
+gsap.registerPlugin(ScrollTrigger);
 export default {
   name: "ServiceStats",
+  mounted() {
+    this.$nextTick(() => {
+      this.initScrollAnimation();
+    });
+    this.statsAnimation();
+  },
+  methods: {
+    statsAnimation() {
+      ScrollTrigger.create({
+        trigger: ".stat-container",
+        start: "top 80%",
+        end: "bottom 20%",
+        scrub: 3,
+        onEnter: () => {
+          gsap.to(".stat-item", {
+            duration: 0.5,
+            opacity: 1,
+            y: 30,
+            stagger: 0.2,
+            ease: "power4.out",
+          });
+        },
+        onLeave: () => {
+          gsap.to(".stat-item", {
+            duration: 0.5,
+            opacity: 0,
+            y: 20,
+            stagger: 0.2,
+            ease: "power4.out",
+          });
+        },
+        onEnterBack: () => {
+          gsap.to(".stat-item", {
+            duration: 0.5,
+            opacity: 1,
+            y: 30,
+            stagger: 0.2,
+            ease: "power4.out",
+          });
+        },
+        onLeaveBack: () => {
+          gsap.to(".stat-item", {
+            duration: 0.5,
+            opacity: 0,
+            y: 20,
+            stagger: 0.2,
+            ease: "power4.out",
+          });
+        },
+      });
+    },
+  },
+  setup() {
+    const stats = inject("dataENG").stats;
+    return {
+      stats,
+    };
+  },
 };
 </script>
