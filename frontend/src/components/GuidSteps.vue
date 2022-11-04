068b8485cacce911f8d8b730b00394604bd89046
NDRBAX <andre.bacaliu@icloud.com>
2022-11-04 02:47:05 +0100

    Tutorial steps to use the service added

--- frontend/src/components/GuidSteps.vue
+++ frontend/src/components/GuidSteps.vue
@@ -1,57 +1,14 @@
 <template>
   <div
-    class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
+    class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24"
   >
-    <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-      <div>
-        <p
-          class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400"
-        >
-          Brand new
-        </p>
-      </div>
-      <h2
-        class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto"
-      >
-        <span class="relative inline-block">
-          <svg
-            viewBox="0 0 52 24"
-            fill="currentColor"
-            class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-          >
-            <defs>
-              <pattern
-                id="f51618fb-0edb-4bcb-b35a-ffc770941286"
-                x="0"
-                y="0"
-                width=".135"
-                height=".30"
-              >
-                <circle cx="1" cy="1" r=".7"></circle>
-              </pattern>
-            </defs>
-            <rect
-              fill="url(#f51618fb-0edb-4bcb-b35a-ffc770941286)"
-              width="52"
-              height="24"
-            ></rect>
-          </svg>
-          <span class="relative">The</span>
-        </span>
-        quick, brown fox jumps over a lazy dog
-      </h2>
-      <p class="text-base text-gray-700 md:text-lg">
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque rem aperiam, eaque ipsa quae.
-      </p>
-    </div>
     <div class="grid gap-8 row-gap-0 lg:grid-cols-3">
       <div class="relative text-center">
         <div
-          class="flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-20 sm:h-20"
+          class="flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-green-50 sm:w-20 sm:h-20"
         >
           <svg
-            class="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16"
+            class="w-12 h-12 text-green-400 sm:w-16 sm:h-16"
             stroke="currentColor"
             viewBox="0 0 52 52"
           >
@@ -72,7 +29,7 @@
         <a
           href="/"
           aria-label=""
-          class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          class="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-deep-purple-800"
           >Learn more</a
         >
         <div
@@ -104,10 +61,10 @@
       </div>
       <div class="relative text-center">
         <div
-          class="flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-20 sm:h-20"
+          class="flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-green-50 sm:w-20 sm:h-20"
         >
           <svg
-            class="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16"
+            class="w-12 h-12 text-green-400 sm:w-16 sm:h-16"
             stroke="currentColor"
             viewBox="0 0 52 52"
           >
@@ -128,7 +85,7 @@
         <a
           href="/"
           aria-label=""
-          class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          class="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-deep-purple-800"
           >Learn more</a
         >
         <div
@@ -160,10 +117,10 @@
       </div>
       <div class="relative text-center">
         <div
-          class="flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-20 sm:h-20"
+          class="flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-green-50 sm:w-20 sm:h-20"
         >
           <svg
-            class="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16"
+            class="w-12 h-12 text-green-400 sm:w-16 sm:h-16"
             stroke="currentColor"
             viewBox="0 0 52 52"
           >
@@ -184,7 +141,7 @@
         <a
           href="/"
           aria-label=""
-          class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          class="inline-flex items-center font-semibold transition-colors duration-200 text-green-400 hover:text-deep-purple-800"
           >Learn more</a
         >
       </div>
