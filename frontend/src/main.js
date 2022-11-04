322bb9493c66b7a5da8d5f9537b204dee6fb40b2
NDRBAX <andre.bacaliu@icloud.com>
2022-11-04 02:46:25 +0100

    Global data setted up

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,33 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import dataENG from "../dataENG";
 // import "./assets/main.css";
 import "./assets/tailwind.css";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import {
+  faShop,
+  faUsers,
+  faMobileScreenButton,
+  faCommentsDollar,
+} from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faShop, faUsers, faMobileScreenButton, faCommentsDollar);
+
 const app = createApp(App);
+app.config.globalProperties.$dataENG = dataENG;
+
+app.provide("dataENG", dataENG);
 
 app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.mount("#app");
