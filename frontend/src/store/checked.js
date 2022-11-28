a3a00e3889094a0a99edfc3cffe4cc6662213bc4
NDRBAX <andre.bacaliu@icloud.com>
2022-11-28 23:09:59 +0100

    Store with Pinia created to handle checkboxes checked for the multistep form

---
+++ frontend/src/store/checked.js
@@ -0,0 +1,24 @@
+// import { defineStore } from "pinia";
+
+// export const useCheckedStore = defineStore("checked", {
+//   // total number of steps
+//   state: () => ({
+//     steps: 0,
+//     // create a store for each step
+//   }),
+// //   create a store for each step
+//     getters: {
+//         getStep: (state) => (step) => {
+
+//   //   getters: {
+//   //     getTotalSteps(state) {
+//   //       console.log("getTotalSteps", state.steps);
+//   //       return state.steps;
+//   //     },
+//   //   },
+//   //   actions: {
+//   //     setTotalSteps(steps) {
+//   //       this.steps = steps;
+//   //     },
+//   //   },
+// });
