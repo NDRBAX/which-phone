05157514895cb47cc410be9a94c69d26bd3df6d4
NDRBAX <andre.bacaliu@icloud.com>
2022-11-29 17:59:14 +0100

    Pinia store updated : setTotalSteps, setCurrentStep and setChecked

--- frontend/src/store/checked.js
+++ frontend/src/store/checked.js
@@ -1,24 +1,42 @@
-// import { defineStore } from "pinia";
+import { defineStore } from "pinia";
 
-// export const useCheckedStore = defineStore("checked", {
-//   // total number of steps
-//   state: () => ({
-//     steps: 0,
-//     // create a store for each step
-//   }),
-// //   create a store for each step
-//     getters: {
-//         getStep: (state) => (step) => {
+export const useCheckedStore = defineStore("checkedValues", {
+  // total number of steps
+  state: () => ({
+    steps: Number,
+    values: [
+      {
+        step: Number,
+        type: String,
+        choices: [],
+      },
+    ],
+  }),
 
-//   //   getters: {
-//   //     getTotalSteps(state) {
-//   //       console.log("getTotalSteps", state.steps);
-//   //       return state.steps;
-//   //     },
-//   //   },
-//   //   actions: {
-//   //     setTotalSteps(steps) {
-//   //       this.steps = steps;
-//   //     },
-//   //   },
-// });
+  getters: {
+    getTotalSteps(state) {
+      console.log("getTotalSteps", state.steps);
+      return state.steps;
+    },
+    // get values
+    getCurrentStepValues(state) {
+      console.log("getCurrentStepValues", state.values);
+      return state.values;
+    },
+    getChecked(state) {
+      console.log("getChecked", state.values);
+      return state.values;
+    },
+  },
+  actions: {
+    setTotalSteps(steps) {
+      this.steps = steps;
+    },
+    setCurrentStep(step) {
+      this.currentStep = step;
+    },
+    setChecked(values) {
+      this.values = values;
+    },
+  },
+});
