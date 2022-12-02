d80d785bd97437c17f29189457dd5c204ce44910
NDRBAX <andre.bacaliu@icloud.com>
2022-12-02 13:54:54 +0100

    Due to system upgrade, python update, files removed

--- backend/phone_chooser/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import wishlist
-
-# Register your models here.
-
-class wishlistAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'link', 'image')
-
-admin.site.register(wishlist, wishlistAdmin)
--- backend/phone_chooser/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import wishlist
-from .serializers import wishlistSerializer
-
-
-class wishlistViewSet(viewsets.ModelViewSet):
-    queryset = wishlist.objects.all()
-    serializer_class = wishlistSerializer
