6ad066390a6a0bbfc8199d1f8d551c66d9291825
NDRBAX <andre.bacaliu@icloud.com>
2022-12-07 15:24:33 +0100

    User can loged into its account and logout

---
+++ wp-frontend/src/views/LoginView.vue
@@ -0,0 +1,141 @@
+<template>
+  <div
+    class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+  >
+    <div class="w-full max-w-md space-y-8">
+      <div>
+        <img
+          class="mx-auto h-12 w-auto"
+          src="../assets/logo.svg"
+          alt="Which Phone"
+        />
+        <h2
+          class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900"
+        >
+          Sign in to your account
+        </h2>
+        <p class="mt-2 text-center text-sm text-gray-600">
+          Or
+          {{ " " }}
+          <a
+            href="/register"
+            class="font-medium text-green-600 hover:text-green-500"
+            >create an account to unclock all features</a
+          >
+        </p>
+      </div>
+      <form class="mt-8 space-y-6" v-on:submit.prevent="submitForm">
+        <input type="hidden" name="remember" value="true" />
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required=""
+              class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
+              placeholder="Email address"
+              v-model="email"
+            />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              required=""
+              class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
+              placeholder="Password"
+              v-model="password"
+            />
+          </div>
+        </div>
+
+        <div class="flex items-center justify-between">
+          <div class="flex items-center">
+            <input
+              id="remember-me"
+              name="remember-me"
+              type="checkbox"
+              class="h-4 w-4 rounded border-gray-300 text-green-600 focus:ring-green-500"
+            />
+            <label for="remember-me" class="ml-2 block text-sm text-gray-900"
+              >Remember me</label
+            >
+          </div>
+
+          <div class="text-sm">
+            <a href="#" class="font-medium text-green-600 hover:text-green-500"
+              >Forgot your password?</a
+            >
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            class="group relative flex w-full justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+          >
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <LockClosedIcon
+                class="h-5 w-5 text-green-500 group-hover:text-green-400"
+                aria-hidden="true"
+              />
+            </span>
+            Sign in
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { LockClosedIcon } from "@heroicons/vue/20/solid";
+import { useAuthStore } from "../stores/auth";
+import axios from "axios";
+
+export default {
+  name: "LoginView",
+  components: {
+    LockClosedIcon,
+  },
+  data() {
+    return {
+      email: "",
+      password: "",
+      errors: {},
+    };
+  },
+  methods: {
+    submitForm() {
+      const formData = {
+        email: this.email,
+        password: this.password,
+      };
+
+      axios.defaults.headers.common["Authorization"] = "";
+
+      axios
+        .post("/auth/token/login/", formData)
+        .then((response) => {
+          const token = response.data.auth_token;
+          console.log(token);
+          const { setToken } = useAuthStore();
+          setToken(token);
+
+          axios.defaults.headers.common["Authorization"] = `Token ${token}`;
+
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
