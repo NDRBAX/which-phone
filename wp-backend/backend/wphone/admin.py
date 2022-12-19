6a3f6fcec52b7219bf400943afc0f0da015e40c9
NDRBAX <andre.bacaliu@icloud.com>
2022-12-19 20:00:53 +0100

    Admin pannel updated

--- wp-backend/backend/wphone/admin.py
+++ wp-backend/backend/wphone/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
-from .models import wishlist, values
+from .models import *
 
-# Register your models here.
+class customUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username',  'is_active', 'is_staff', 'is_admin')
+
 class wishlistAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'link', 'image')
+    list_display = ('user', 'name', 'price', 'link', 'image')
 
 class valuesAdmin(admin.ModelAdmin):
-    list_display = ('step', 'label', 'checked')
-admin.site.register(wishlist, wishlistAdmin)
-admin.site.register(values, valuesAdmin)
+    list_display = ('user', 'checked')
+
+class brandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url', 'num_devices')
+
+class deviceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'brand_id', 'name', 'url', 'thumbnail', 'summary')
+
+class device_specificationsAdmin(admin.ModelAdmin):
+    list_display = ('device_id', 'spec_id', 'spec_category', 'specification', 'spec_value')
+
+admin.site.register(Wishlist, wishlistAdmin)
+admin.site.register(Values, valuesAdmin)
+admin.site.register(CustomUser, customUserAdmin)
+admin.site.register(Brand, brandAdmin)
+admin.site.register(Device, deviceAdmin)
+admin.site.register(Device_specification, device_specificationsAdmin)
+
