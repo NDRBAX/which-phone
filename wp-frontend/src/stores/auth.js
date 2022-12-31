95c731b4c587021f6882f61d3f0d65e792ad2393
NDRBAX <andre.bacaliu@icloud.com>
2022-12-31 01:30:55 +0100

    Store for authentification done

--- wp-frontend/src/stores/auth.js
+++ wp-frontend/src/stores/auth.js
@@ -4,11 +4,12 @@ import { defineStore } from "pinia";
 export const useAuthStore = defineStore({
   id: "auth",
   state: () => ({
-    // token: null,
-    // isAuthenticated: false,
     access: null,
     refresh: null,
     username: null,
+    avatar: null,
+    email: null,
+    historyCount: null,
   }),
   getters: {
     getAccess: (state) => {
@@ -20,6 +21,15 @@ export const useAuthStore = defineStore({
     getUser: (state) => {
       return state.username;
     },
+    getAvatar: (state) => {
+      return state.avatar;
+    },
+    getEmail: (state) => {
+      return state.email;
+    },
+    getHistoryCount: (state) => {
+      return parseInt(state.historyCount);
+    },
   },
 
   actions: {
@@ -27,15 +37,24 @@ export const useAuthStore = defineStore({
       let accessToken = localStorage.getItem("access");
       let refreshToken = localStorage.getItem("refresh");
       let userName = localStorage.getItem("username");
+      let avatar = localStorage.getItem("avatar");
+      let email = localStorage.getItem("email");
+      let historyCount = localStorage.getItem("historyCount");
 
       if (accessToken) {
         this.access = accessToken;
         this.refresh = refreshToken;
         this.username = userName;
+        this.avatar = avatar;
+        this.email = email;
+        this.historyCount = historyCount;
       } else {
         this.access = null;
         this.refresh = null;
         this.username = null;
+        this.avatar = null;
+        this.email = null;
+        this.historyCount = null;
       }
     },
 
@@ -49,12 +68,30 @@ export const useAuthStore = defineStore({
       localStorage.removeItem("access");
       localStorage.removeItem("username");
       localStorage.removeItem("refresh");
+      localStorage.removeItem("avatar");
+      localStorage.removeItem("email");
+      localStorage.removeItem("historyCount");
       console.log("access removed");
     },
 
     setUser(username) {
       this.username = username;
       localStorage.setItem("username", username);
+    },
+
+    setAvatar(avatar) {
+      this.avatar = avatar;
+      localStorage.setItem("avatar", avatar);
+    },
+
+    setEmail(email) {
+      this.email = email;
+      localStorage.setItem("email", email);
+    },
+
+    setHistoryCount(count) {
+      this.historyCount = count;
+      localStorage.setItem("historyCount", count);
     },
 
     setRefresh(refresh) {
